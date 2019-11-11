449d5aef5f91b01cc6f6498251d71799f50c1892
Yadav <rayadav@internal.tavisca.com>
2019-11-11 10:14:57 +0530

    Endpoints Implemented

---
+++ Game.Service/TikTacToe.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Game.Service
+{
+    public class TikTacToe
+    {
+        public static char[,] board = new char[3, 3] { { ' ', ' ', ' ' },
+        { ' ',' ',' '},
+        { ' ',' ',' '} };
+
+        public static bool gameOver = false;
+
+        public static bool CheckPlayerWin(char symbol)
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                if (board[i, 0] == symbol && board[i, 1] == symbol && board[i, 2] == symbol)
+                    return true;
+
+                if (board[0, i] == symbol && board[1, i] == symbol && board[2, i] == symbol)
+                    return true;
+            }
+            if (board[0, 0] == symbol && board[1, 1] == symbol && board[2, 2] == symbol)
+                return true;
+
+            if (board[0, 2] == symbol && board[1, 1] == symbol && board[2, 0] == symbol)
+                return true;
+
+            return false;
+        }
+
+    }
+}
---
+++ Game/Models/Move.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Game.Models
+{
+    public class Move
+    {
+        public int Player { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+
+    }
+}
